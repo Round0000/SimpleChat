3d95f5868e50703a2ec230099763c4b85ceefb0e
Round0000 <billybinche@gmail.com>
2022-02-09 23:04:15 +0100

    commit

--- app.js
+++ app.js
@@ -1,21 +1,33 @@
 message_form.addEventListener("submit", (e) => {
   e.preventDefault();
 
-  updateChatUI(message_form_textarea.value);
+  if (message_form_textarea.value.length === 0) return;
+
+  updateChatUI(message_form_textarea.value, true);
 });
 
 message_form.addEventListener("keyup", (e) => {
   if (e.key === "Enter" && !e.shiftKey) {
+    console.log(message_form_textarea.value.length);
+    if (message_form_textarea.value.length <= 1) return;
+
     updateChatUI(message_form_textarea.value);
   }
 });
 
-function updateChatUI(msg, side) {
+function updateChatUI(msg, self) {
   message_form.reset();
 
   const li = document.createElement("LI");
   li.innerText = msg;
-  li.classList.add("message", "message_left");
+  li.classList.add("message", "message_out");
+
+  if (self) {
+    li.classList.add("message_left");
+  } else {
+    li.classList.add("message_right");
+  }
+
   const timestamp = document.createElement("SPAN");
   let time = Date.now();
   time = new Date(time);
@@ -27,4 +39,5 @@ function updateChatUI(msg, side) {
   li.append(timestamp);
   messages_list.append(li);
   message_form.scrollIntoView();
+  li.classList.remove("message_out");
 }
